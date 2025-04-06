01f5cf9f1a7b5aa8c45760d128243248da04145e
Gouri <gouri@umich.edu>
2025-04-05 21:46:57 -0400

    Update cli.py

--- microbiome_bmi_classifier/cli.py
+++ microbiome_bmi_classifier/cli.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import argparse
-from data_processing import load_data, preprocess_data, split_data
+from .data_processing import load_data, preprocess_data, split_data
 from feature_extraction import extract_features
 from classification import train_model, evaluate_model, cross_validate_model
 from synthetic_data import create_synthetic_data
